5517244f8895a3e54c9b69eb5d3ad426eb74e27d
Avinash_Kumar <85475354+Avinash-Kumarr@users.noreply.github.com>
2021-09-24 13:14:11 +0530

    Todo-APP-V1.0
    Todo App

---
+++ src/app/Todo.ts
@@ -0,0 +1,8 @@
+export class Todo{
+    sno: number= 8;
+    title: string= "avi";
+    desc: string="avi";
+    active: boolean= true;
+    
+    
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Todo-App';
+  // constructor(){
+  //     this.title='Avinash';
+  // 
+}
+// let n = new AppComponent();
